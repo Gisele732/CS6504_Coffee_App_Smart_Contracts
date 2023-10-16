8ee9caf380653666f434925c5e30cab3418442a7
Anne-Lise Mornard-Stott <99225221+Gisele732@users.noreply.github.com>
2023-10-16 20:38:52 +1300

    Update ProducerRegistration.sol
    Changed "Cultivation Method" to "Processing Method"

--- ProducerRegistration.sol
+++ ProducerRegistration.sol
@@ -2,11 +2,8 @@
 // Purpose: 
 // - Allows coffee producers to register their product.
 // Key Features:
-// - Register a new batch of coffee with details like origin, method of cultivation, harvest date, and any certifications.
-// - Update details of a batch if necessary.
+// - Register a new batch of coffee with details like origin, method of processing, harvest date, and any certifications.
 // - Retrieve details of a batch.
-
-// TODO: Create a batch detail update function
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
@@ -16,7 +13,7 @@ contract ProducerRegistration {
     // characteristics of a coffee batch for the purpose of tracking
     struct CoffeeBatch {
         string origin;
-        string cultivationMethod;
+        string processingMethod;
         uint256 harvestDate;
         string certification;
         bool isRegistered; //should this be set to false by default?
@@ -29,14 +26,14 @@ contract ProducerRegistration {
     function registerBatch(
         string memory _batchId,
         string memory _origin,
-        string memory _cultivationMethod,
+        string memory _processingMethod,
         uint256 _harvestDate,
         string memory _certification
     ) public {
         require(!coffeeBatches[_batchId].isRegistered, "Batch ID already registered.");
         coffeeBatches[_batchId] = CoffeeBatch({
             origin: _origin,
-            cultivationMethod: _cultivationMethod,
+            processingMethod: _processingMethod,
             harvestDate: _harvestDate,
             certification: _certification,
             isRegistered: true
@@ -47,29 +44,8 @@ contract ProducerRegistration {
     function getBatchDetails(string memory _batchId) public view returns (string memory, string memory, uint256, string memory) {
         require(coffeeBatches[_batchId].isRegistered, "Batch ID not found.");
         CoffeeBatch memory batch = coffeeBatches[_batchId];
-        return (batch.origin, batch.cultivationMethod, batch.harvestDate, batch.certification);
+        return (batch.origin, batch.processingMethod, batch.harvestDate, batch.certification);
     }
-    
-    // Helper function to convert a uint256 to a string
-    function uintToString(uint256 value) internal pure returns (string memory) {
-        if (value == 0) {
-            return "0";
-        }
-        uint256 temp = value;
-        uint256 digits;
-        while (temp != 0) {
-            digits++;
-            temp /= 10;
-        }
-        bytes memory buffer = new bytes(digits);
-        while (value != 0) {
-            digits -= 1;
-            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
-            value /= 10;
-        }
-        return string(buffer);
-    }
-
 
     function isBatchRegistered(string memory _batchId) public view returns (bool) {
     return coffeeBatches[_batchId].isRegistered;
